2486e598737ebc1e4e084f99fc0d8d9148cb43a0
Favour <pharlorlah700@gmail.com>
2023-03-18 09:22:37 +0100

    quest1

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 // welcome the user
 alert("Welcome to Quiz Ninja!");
-var name = "final";
-console.log
+
+var question = "Who will win Lagos State election?"
+var answer = prompt(question);
+alert("You answered" + answer );
